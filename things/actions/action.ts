4c733f140dbdc6add75425820981a7232b1f161f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-20 14:28:52 +0900

    chore(things): move thing to things dir

---
+++ things/actions/action.ts
@@ -0,0 +1,17 @@
+import { ThingProps, ThingType } from "../thing.ts";
+
+export interface ActionProps extends ThingProps {
+  /** For failed actions, more information on the cause of the failure.
+   * @see https://schema.org/error
+   */
+  error: ThingType;
+}
+
+/** An action performed by a direct agent and indirect participants upon a direct object. Optionally happens at a location with the help of an inanimate instrument. The execution of the action may produce a result. Specific action sub-type documentation specifies the exact expectation of each argument/role.
+ * @see {@link http://blog.schema.org/2014/04/announcing-schemaorg-actions.html blog post} and {@link http://blog.schema.org/2014/04/announcing-schemaorg-actions.html Actions overview document}.
+ */
+export interface Action extends ActionProps {
+  "@type": "Action";
+}
+
+export type ActionType = Action;
---
+++ things/events/event.ts
@@ -0,0 +1,14 @@
+import { ThingProps, ThingType } from "../thing.ts";
+
+export interface EventProps extends ThingProps {
+  about: ThingType;
+}
+
+/** An event happening at a certain time and location, such as a concert, lecture, or festival. Ticketing information may be added via the {@link Offers} property. Repeated events may be structured as separate Event objects.
+ * @see https://schema.org/Event
+ */
+export interface Event extends EventProps {
+  "@type": "Event";
+}
+
+export type EventType = Event;
