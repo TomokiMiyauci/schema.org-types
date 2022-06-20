d1ed59eeef49114916bec01c49a0da19d97b5a14
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-20 16:30:20 +0900

    chore: export `Book` types

--- schema_org.ts
+++ schema_org.ts
@@ -1,6 +1,7 @@
 import { Thing } from "./things/thing.ts";
 import { BreadcrumbList } from "./things/creative_works/item_lists/breadcrumb_list.ts";
 import { WebPage } from "./things/creative_works/web_page.ts";
+import { Book } from "./things/creative_works/books/book.ts";
 
 type DeepPartial<T> = {
   readonly [P in keyof T]?: keyof T[P] extends never ? T[P] : DeepPartial<T[P]>;
@@ -12,6 +13,7 @@ type SchemaOrg =
     | DeepPartial<Thing>
     | DeepPartial<BreadcrumbList>
     | DeepPartial<WebPage>
+    | DeepPartial<Book>
   );
 
 export default SchemaOrg;
