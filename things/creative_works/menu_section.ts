82dbff9f7baccfb0bc14690796d28f188e0a0d56
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-06-22 15:23:10 +0900

    docs(things): add docs

--- things/creative_works/menu_section.ts
+++ things/creative_works/menu_section.ts
@@ -1,9 +1,17 @@
-import { MenuItem } from "./../intangibles/menu_item";
+import { MenuItem } from "../intangibles/menu_item.ts";
 import { CreativeWorkProps } from "./creative_work.ts";
 import { ThingProps } from "../thing.ts";
 
 export interface MenuSectionProps extends CreativeWorkProps, ThingProps {
+  /** A food or drink item contained in a menu or menu section.
+   * @see https://schema.org/hasMenuItem
+   */
   hasMenuItem: MenuItem;
+
+  /** A subgrouping of the menu (by dishes, course, serving time period, etc.).
+   * @see https://schema.org/hasMenuSection
+   */
+  hasMenuSection: MenuSection;
 }
 
 /** A sub-grouping of food or drink items in a menu. E.g. courses (such as 'Dinner', 'Breakfast', etc.), specific type of dishes (such as 'Meat', 'Vegan', 'Drinks', etc.), or some other classification made by the menu provider.
